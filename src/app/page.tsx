592d47f1d768d2332799e7c9c1de9c9e447f9efc
vitmart <myexperiment787@gmail.com>
2026-01-27 02:17:36 +0530

    Remove duplicate app folder and fix routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from "../components/Navbar";
 import ProductCard from "../components/ProductCard";
 
 const products = [
@@ -8,10 +9,13 @@ const products = [
 
 export default function Home() {
   return (
-    <div className="p-4 grid grid-cols-1 md:grid-cols-3 gap-4">
-      {products.map((p, i) => (
-        <ProductCard key={i} name={p.name} price={p.price} />
-      ))}
-    </div>
+    <>
+      <Navbar />
+      <div className="p-4 grid grid-cols-1 md:grid-cols-3 gap-4">
+        {products.map((p, i) => (
+          <ProductCard key={i} name={p.name} price={p.price} />
+        ))}
+      </div>
+    </>
   );
 }
